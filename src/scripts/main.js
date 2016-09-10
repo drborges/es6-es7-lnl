2ccc6e9de0482c21ab696be870e12937895588f1
Diego Borges <drborges.cic@gmail.com>
2016-09-10 13:23:57 -0300

    Add valtaire theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-cube'),
+  voltaire = require('bespoke-theme-voltaire'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
@@ -12,7 +12,7 @@ var bespoke = require('bespoke'),
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
+  voltaire(),
   keys(),
   touch(),
   bullets('li, .bullet'),
@@ -27,4 +27,3 @@ bespoke.from('article', [
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
-
