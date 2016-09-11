9665e877ac83ac1b51942d0ed727d4c09817b708
Diego Borges <drborges.cic@gmail.com>
2016-09-10 23:10:47 -0300

    Add support to async functions

--- src/scripts/bespoke-babel-transpiler.js
+++ src/scripts/bespoke-babel-transpiler.js
@@ -5,6 +5,9 @@ var options = {
     require("babel-preset-es2016"),
     require("babel-preset-es2017"),
     require("babel-preset-stage-0"),
+  ],
+  plugins: [
+    require("babel-plugin-transform-async-to-generator"),
   ]
 }
 
