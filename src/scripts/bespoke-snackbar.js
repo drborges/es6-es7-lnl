2ee2c6f19fe2e57b641ee1044ae181ed2aec9837
Diego Borges <drborges.cic@gmail.com>
2016-09-13 23:53:51 -0300

    Only display first line of error message on snackbar

--- src/scripts/bespoke-snackbar.js
+++ src/scripts/bespoke-snackbar.js
@@ -6,7 +6,7 @@ function hide(snackbar) {
 
 function show(snackbar, message) {
   if (snackbar) {
-    snackbar.textContent = message
+    snackbar.textContent = message.split("\n")[0]
     snackbar.classList.remove('hidden')
   }
 }
@@ -17,12 +17,10 @@ module.exports = function(options) {
 
   var plugin = function() {
     return function(deck) {
-      window.addEventListener('load', function() {
-        snackbar = document.querySelector(options.snackbarSelector)
-        deck.on('next', function() { hide(snackbar) })
-        deck.on('prev', function() { hide(snackbar) })
-        deck.on('slide', function() { hide(snackbar) })
-      })
+      snackbar = document.querySelector(options.snackbarSelector)
+      deck.on('next', function() { hide(snackbar) })
+      deck.on('prev', function() { hide(snackbar) })
+      deck.on('slide', function() { hide(snackbar) })
     }
   }
 
@@ -33,7 +31,7 @@ module.exports = function(options) {
       hide(snackbar)
       fn()
     } catch(e) {
-      show(snackbar, e)
+      show(snackbar, e.message)
       timeout = setTimeout(function() { hide(snackbar) }, 5000)
     }
   }
